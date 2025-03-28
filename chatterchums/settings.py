3de7b47a8ffaefefab386147d4fda4d53d43f3d5
Anna Kuznetsova <3061786K@student.gla.ac.uk>
2025-03-28 11:53:37 +0000

    Set allowed hosts in settings.py

--- chatterchums/settings.py
+++ chatterchums/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'django-insecure-uje=wb95nvsmk)sbbvyft3988t$#bvt-baspgi%y-tbfmwwwly
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['aniavk.pythonanywhere.com', 'localhost', '127.0.0.1']
 
 
 
